43fad525a35ada77a027020e6a314614a4b4c8d9
ibrag8998 <ibragdzh@gmail.com>
2020-08-11 22:52:44 +0300

    v0.0.2 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="chaban",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     author="Ibrahim Gadzhimagomedov",
     author_email="ibragdzh@gmail.com",
