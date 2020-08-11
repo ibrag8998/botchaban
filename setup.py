465f1cb6a915ed9197a07e72aaeaeff07c1e359a
ibrag8998 <ibragdzh@gmail.com>
2020-08-11 22:58:48 +0300

    setup.py - add install_requires option

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import find_packages, setup
 with open("README.md") as f:
     long_description = f.read()
 
+with open("requirements/base.txt") as f:
+    base_requirements = f.readlines()
+
 setup(
     name="chaban",
     version="0.0.2",
@@ -12,10 +15,11 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ibrag8998/chaban/",
+    python_requires=">=3.8",
+    install_requires=base_requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
 )
